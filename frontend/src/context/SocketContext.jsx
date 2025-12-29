85b7d3ffc95728086a2ce3399e9ab8f32ae13e6f
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2025-12-29 17:16:46 +0530

    refactor: remove unnecessary semicolon in SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useAuthContext } from "./AuthContext";
 import io from "socket.io-client"
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || window.location.origin;
-export const SocketContext = createContext();
+export const SocketContext = createContext()
 
 export const useSocketContext = () => {
 	return useContext(SocketContext);
